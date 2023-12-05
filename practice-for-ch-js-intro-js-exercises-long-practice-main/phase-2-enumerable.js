12f4ae5b250ab634cafd41e9f4b8e17d7594118c
Ishan Chawla <ishan.kpv@gmail.com>
2023-12-05 14:39:25 -0500

    phase 2 COMPLETE

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,24 @@
 // Your code here
+
+Array.prototype.myEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+};
+
+Array.prototype.myMap = function (callback) {
+    const newArr = [];
+    this.myEach(function (e) {
+        newArr.push(callback(e));
+    });
+    return newArr;
+};
+
+Array.prototype.myReduce = function (callback, initialValue = this[0]) {
+    let acc = initialValue;
+    this.myEach((e) => {
+        acc = callback(acc, e);
+    });
+    return acc;
+};
+
